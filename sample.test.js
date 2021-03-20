e8e7187c10300d25306f07397295cd3a6760638a
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2021-03-21 00:56:42 +0200

    test string2DOM, using axios

---
+++ sample.test.js
@@ -0,0 +1,14 @@
+// const request = require('supertest')('https://www.f-secure.com/en');
+const supertest = require('supertest');
+const axios = require('axios');
+
+const api = supertest('https://google.com/');
+
+test('notes are returned as json', async () => {
+  axios.get('https://google.com/').then(function (response) {
+    // handle success
+    console.log(response);
+  });
+  await api.get('').expect(200);
+  // .expect('Content-Type', /application\/json/);
+});
